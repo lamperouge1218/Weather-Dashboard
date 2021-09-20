73403e9d37acd04c8c50d56f03f5cdf81670ebb4
lamperouge <philbohn1791@gmail.com>
2021-09-20 10:02:33 -0400

    starting to connect API to JS page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,26 @@
 var APIkey = "22699aa722b7a79950a6f5dfaa6a318e";
+var inputField = $(".inputfield");
+var searchButton = $(".searchBtn");
+var ulEl = $(".ul");
+var form = $(".input-group");
 
+function getAPI() {
+    var city = $("input").val();
+    var requestURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
+}
+// GIVEN a weather dashboard with form inputs
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
